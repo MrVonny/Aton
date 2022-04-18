dfeb7a4cd7495052cf861c82f8895d382f37363d
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-04-19 01:30:08 +0300

    Initial Controller & User Model

---
+++ Aton/Database/AtonDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Aton.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aton.Database;
+
+public sealed class AtonDbContext : IdentityDbContext<User>
+{
+    public AtonDbContext(DbContextOptions<AtonDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    
+}
