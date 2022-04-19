72a6ce3ca620e8bf12ba937fa9f38cc2c77b5c0c
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-04-19 03:02:42 +0300

    Unit tests

---
+++ AtonTest/TestBase.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using Aton.Database;
+using Aton.Models.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace AtonTest;
+
+public abstract class TestBase
+{
+    protected AtonDbContext AtonDbContext;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<AtonDbContext>()
+            .UseInMemoryDatabase(databaseName: "Test")
+            .Options;
+        IList<User> entities = new List<User> { 
+            new()
+            {
+                Admin = false,
+                Birthday = DateTime.Parse("1992-04-18"),
+                Gender = Gender.Female,
+                UserName = "Козлова Марина Викторовна",
+                Login = "Koz123",
+                CreatedOn = DateTime.Parse("2021-11-04 22:13:46"),
+                
+            }, 
+            new()
+            {
+                Admin = false,
+                Birthday = DateTime.Parse("1994-10-04"),
+                Gender = Gender.Male,
+                UserName = "Брайко Иван Сергеевич",
+                Login = "Bis010",
+                CreatedOn = DateTime.Parse("2021-04-01 02:53:12"),
+                
+            }, 
+            new()
+            {
+                Admin = true,
+                Birthday = DateTime.Parse("1970-12-10"),
+                Gender = Gender.Unknown,
+                UserName = "Очень важный человек",
+                Login = "Admin10",
+                CreatedOn = DateTime.Parse("2002-01-02 12:00:02"),
+                
+            }, 
+        };
+        AtonDbContext = new AtonDbContext(options);
+        AtonDbContext.Users.AddRange(entities);
+        AtonDbContext.SaveChanges();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        AtonDbContext.Dispose();
+    }
+    
+    
+}
---
+++ AtonTest/UserExtensionsTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Aton.Models.Identity;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace AtonTest;
+
+[TestFixture]
+public class UserExtensionsTest : TestBase
+{
+    [Test]
+    public async Task ToIndexModelTest()
+    {
+        var user = await AtonDbContext.Users.FirstAsync();
+        var indexUser = user.ToIndexModel();
+        Assert.AreEqual(user.Admin, indexUser.Admin);
+        Assert.AreEqual(user.Id, indexUser.Id);
+        Assert.AreEqual(user.Login, indexUser.Login);
+        Assert.AreEqual(user.CreatedOn, indexUser.CreatedOn);
+        Assert.AreEqual(user.UserName, indexUser.UserName);
+    }
+
+    [Test]
+    public async Task ToIndexModelQueryTest()
+    {
+        var users = await AtonDbContext.Users
+            .ToIndexModel()
+            .ToListAsync();
+
+        Assert.AreEqual(await AtonDbContext.Users.CountAsync(), users.Count);
+        Assert.That(users, Is.All.Property("Id").Not.Null);
+        Assert.That(users, Is.All.Property("Login").Not.Null);
+    }
+
+    [Test]
+    [TestCase(10)]
+    [TestCase(20)]
+    [TestCase(50)]
+    public async Task OlderThanTest(int olderThan)
+    {
+        var users = await AtonDbContext.Users
+            .OlderThan(olderThan)
+            .ToListAsync();
+        foreach (var user in users)
+        {
+            var years = DateTime.Now.Year - user.Birthday!.Value.Year;
+            var birthdayThisYear = user.Birthday.Value.AddYears(years);
+            var age = birthdayThisYear > DateTime.Now ? years - 1 : years;
+            Assert.Greater(age, olderThan);
+        }
+    }
+}
