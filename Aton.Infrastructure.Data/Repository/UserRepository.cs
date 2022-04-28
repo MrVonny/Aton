4a837278b63e5afe4a3f515b223aef31fa9bbdf1
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-04-28 17:26:16 +0300

    GetOlderThan - refactoring

--- Aton.Infrastructure.Data/Repository/UserRepository.cs
+++ Aton.Infrastructure.Data/Repository/UserRepository.cs
@@ -14,19 +14,23 @@ public class UserRepository : Repository<User>, IUserRepository
     public async Task<IEnumerable<User>> GetActiveOrdered()
     {
         return await DbSet
-            .Where(u => !u.RevokedAt.HasValue && !u.RevokedAt.HasValue)
+            .Where(u => !u.RevokedAt.HasValue && u.RevokedBy == null)
             .OrderBy(u => u.CreatedAt)
             .ToListAsync();
     }
 
     public async Task<IEnumerable<User>> GetOlderThan(int olderThan)
     {
+        // return await DbSet.Where(u =>
+        //     u.Birthday.HasValue &&
+        //     (u.Birthday.Value.AddYears(DateTime.Now.Year - u.Birthday.Value.Year) >
+        //      DateTime.Now
+        //         ? DateTime.Now.Year - u.Birthday.Value.Year - 1
+        //         : DateTime.Now.Year - u.Birthday.Value.Year) > olderThan)
+        //     .ToListAsync();
         return await DbSet.Where(u =>
             u.Birthday.HasValue &&
-            (u.Birthday.Value.AddYears(DateTime.Now.Year - u.Birthday.Value.Year) >
-             DateTime.Now
-                ? DateTime.Now.Year - u.Birthday.Value.Year - 1
-                : DateTime.Now.Year - u.Birthday.Value.Year) > olderThan)
+            u.Birthday.Value.AddYears(olderThan) <= DateTime.Now)
             .ToListAsync();
     }
 }
